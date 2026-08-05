4676a2c9ddeb3a132d73fcc462ff03619e2c495e
Supun hasanka <38599870+supun2001@users.noreply.github.com>
2026-08-05 01:15:50 +0100

    create a project

---
+++ project/frontend/src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
